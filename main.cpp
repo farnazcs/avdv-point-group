7a3bbae2405de7e35b6f027a9abdafa697289fa4
Farnazcs <Farnazcs@github.com>
2019-11-22 18:42:09 -0800

    Lattice class

--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
 #include "Eigen/Dense"
+#include "Eigen/src/Core/Matrix.h"
 #include <iostream>
+
+class Lattice {
+
+    public:
+Lattice (Eigen::Matrix3d mat):
+    column_mat (mat)
+    {
+    }
+Eigen::Vector3d a()
+{
+    return column_mat.col(0);
+}
+
+    private: 
+        Eigen::Matrix3d column_mat;
+
+
+};
 
 int main()
 {
-  return 0;
+    Eigen::Matrix3d test_matrix;
+test_matrix << 1,0,0,
+               0,1,0,
+               0,0,1;
+Lattice bcc(test_matrix);
+std::cout<<bcc.a();
+    return 0;
 }  
